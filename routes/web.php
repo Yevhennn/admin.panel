c9da2d2024623b02aa380997e7bd0656f0fcae3e
Yevhennn <eugenefun@hotmail.com>
2026-06-28 00:31:59 +0300

    no redirect

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DefaultController;
 
-Route::redirect('/', '/admin');
-
 Route::get('/reviews', [DefaultController::class, 'index'])->name('reviews.index');
 
 if (file_exists(__DIR__.'/backpack/custom.php')) {
